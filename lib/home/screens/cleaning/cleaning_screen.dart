9ede424c971c10588f4606f7198209cc6b5d5e03
PC <embakytbekov@gmail.com>
2025-11-30 23:35:06 -0800

    save02

--- lib/home/screens/cleaning/cleaning_screen.dart
+++ lib/home/screens/cleaning/cleaning_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:eliman/home/screens/cleaning/cleaning_details_screen.dart';
 import 'package:eliman/home/services/cleaning_services.dart';
+import 'package:eliman/home/screens/cleaning/cleaning_details_screen.dart';
 import 'package:eliman/home/screens/cleaning/widgets/service_card.dart';
 
 class CleaningScreen extends StatelessWidget {
@@ -11,30 +11,17 @@ class CleaningScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.white,
         elevation: 0,
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.black),
         title: const Text(
           "Services",
           style: TextStyle(
             color: Colors.black,
             fontSize: 22,
-            fontWeight: FontWeight.w600,
+            fontWeight: FontWeight.w700,
           ),
         ),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 16),
-            child: Center(
-              child: Text(
-                "14 services",
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 15,
-                ),
-              ),
-            ),
-          ),
-        ],
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -48,6 +35,7 @@ class CleaningScreen extends StatelessWidget {
           ),
           itemBuilder: (context, index) {
             final item = cleaningServices[index];
+
             return ServiceCard(
               item: item,
               onTap: () {
